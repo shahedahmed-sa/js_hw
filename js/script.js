d352c13f28596b7ea0b98f2ee9f777ed7000e3d5
Kazi Shahed Ahmed <123163227+shahedahmed-sa@users.noreply.github.com>
2023-06-02 17:22:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+  /*heading change*/
+ function textChange()
+ {
+    document.getElementById('h2').innerHTML="SSB-515, Minhaz Faisal, Lead Mentor, Full Stack Web Development";
+ }
+ /*heading change*/
+
+
+ /*bulb on off*/
+
+ function bulbOn()
+ {
+    document.getElementById('bulb').src="images/on.gif";
+ }
+
+ function bulbOff()
+ {
+    document.getElementById('bulb').src="images/off.gif";
+ }
+
+ /*bulb on off*/
+
+ /*date*/
+ function date()
+ {
+    document.getElementById('showdate').innerHTML=Date();
+ }
